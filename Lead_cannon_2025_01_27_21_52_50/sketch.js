bea7b2cb8d2865cd22a7d94e37112305249a382d
oliviahanley18 <olivia.hanley@maine.edu>
2025-01-27 16:53:34 -0500

    Add files via upload

---
+++ Lead_cannon_2025_01_27_21_52_50/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("hsl(180,80%,50%)");
+  fill("rgb(172,235,172)")
+  triangle(100,100,200,180,177,60)
+  fill("rgb(163,235,163)")
+  ellipse(100,100,100,50)
+  
+  
+  triangle(90,100,120,100,120,120)
+  
+  fill("white")
+  circle(70,100,20);
+  fill("black")
+  circle(70,100,10);
+  
+ 
+  
+  
+  
+
+  
+}
